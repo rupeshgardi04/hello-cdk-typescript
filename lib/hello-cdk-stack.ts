e50e89f33525abc03592051916e2aa4bc27f0b15
rupesh-tangible <rupesh@tangible.tech>
2022-02-27 16:00:27 +0530

    rest api endpoint with lambda

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,17 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { join } from "path";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { handler } from "../restServices/node-lambda/hello";
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
+      entry: join(__dirname, "..", "restServices", "node-lambda", "hello.ts"),
+      handler: "handler",
+    });
   }
 }
